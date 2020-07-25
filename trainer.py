69a8adb30d2e23085bd9ba3048229709fcd4e49e
null <alexanderalecchang@gmail.com>
2020-07-25 01:59:09 -0700

    trainer.py

--- trainer.py
+++ trainer.py
@@ -1,6 +1,23 @@
+import sys
+import pickle
+
+
 def main():
+    phrasefile = open(sys.argv[1])
+    phraselist = []
+
+    for phrase in phrasefile:
+        print(phrase + "\n(y/n): ", end="")
+        if(input().lower() == "y"):
+            phraselist.append((phrase, 1))
+        else:
+            phraselist.append((phrase, 0))
 
 
+    listdump = open("listdump.pkl", "wb")
+    pickle.dump(phraselist, listdump)
+    listdump.close()
+    print("all done :)")
 
 if __name__ == "__main__":
     main()
