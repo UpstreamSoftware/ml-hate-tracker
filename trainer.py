79e2dc73d50cf2d353da3d026ee73f342e7cf0a7
null <alexanderalecchang@gmail.com>
2020-07-25 02:44:35 -0700

    added trainer

--- trainer.py
+++ trainer.py
@@ -1,23 +1,32 @@
 import sys
 import pickle
+import codecs
 
 
 def main():
-    phrasefile = open(sys.argv[1])
+    phrasefile = codecs.open(sys.argv[1], "r", "ISO-8859-1")
     phraselist = []
 
-    for phrase in phrasefile:
-        print(phrase + "\n(y/n): ", end="")
-        if(input().lower() == "y"):
-            phraselist.append((phrase, 1))
-        else:
-            phraselist.append((phrase, 0))
+    line_num = 0
 
+    for phrase in phrasefile:
+        print(str(line_num) + "| " + phrase + "(y/n) or q to quit: ", end="")
+        selection = input()
+        while(selection not in "ynq"):
+            selection = input()
+        if selection.lower() == "y":
+            phraselist.append(tuple((phrase, 1)))
+        elif selection.lower() == "n":
+            phraselist.append(tuple((phrase, 0)))
+        elif selection.lower() == "q":
+            break
+        line_num += 1
 
     listdump = open("listdump.pkl", "wb")
     pickle.dump(phraselist, listdump)
     listdump.close()
     print("all done :)")
+
 
 if __name__ == "__main__":
     main()
